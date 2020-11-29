d021de8bd4533a49c594aef72c0f2c27be938aed
Brown Mamba <brdyherrera@gmail.com>
2020-11-29 17:56:45 -0500

    added to the form in HTML and added variables in JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Variables for the elements on the page
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const button = document.querySelector("button");
+const input = document.getElementById("state-input");
+
+// Create an li element
+// const liMaker = (text) => {
+//   const li = document.createElement("li");
+//   li.textContent = text;
+//   ul.appendChild(li);
+// };
+
+// Event listener to listen for the submit on the form
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   liMaker(input.value);
+//   input.value = "";
+// });
